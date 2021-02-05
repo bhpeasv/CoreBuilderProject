5b9267c32c8ea4a8ce3a6f5e554794eb9f04d934
BentHPedersen <benthpedersen@mail.com>
2021-02-05 09:28:52 +0100

    Add project files.

---
+++ CoreBuilderProject/MyBuildManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBuilderProject
+{
+    public class MyBuildManager
+    {
+        private Dictionary<string, BuildAgent> agents = new Dictionary<string, BuildAgent>();
+
+        public void AddAgent(string agentId, string solutionPath, int intervalSeconds)
+        {
+            if (agents.ContainsKey(agentId))
+                throw new ArgumentException("Agent id already in use [{0}]", agentId);
+            agents[agentId] = new BuildAgent(agentId, solutionPath, intervalSeconds);
+        }
+
+        public void RemoveAgent(string agentId)
+        {
+            if (!agents.ContainsKey(agentId))
+                throw new ArgumentException("Unknown Agent id [{0}]", agentId);
+            agents[agentId].Cancel();
+            agents.Remove(agentId);
+        }
+    }
+}
---
+++ CoreBuilderProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoreBuilderProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter your own project paths
+            var filename = @"c:\users\bhp\source\repos\SDM\YetAnotherBank";
+            
+            var buildManager = new MyBuildManager();
+            buildManager.AddAgent("Agent1", filename, 5);
+            //buildManager.AddAgent("Agent2", filename, 10);
+
+            //Console.Write("Press any key to stop agent...");
+            Console.ReadKey();
+
+            buildManager.RemoveAgent("Agent1");
+
+            //Console.ReadKey();
+            //buildManager.RemoveAgent("Agent2");
+
+            Console.WriteLine("Agents canceled");            
+        }
+    }
+}
